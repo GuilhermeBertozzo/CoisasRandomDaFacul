8beb0aba2f3a791aa173ef5a4674812d8ece21ec
GuilhermeBertozzo <guibertozzo@hotmail.com>
2020-09-15 14:51:54 -0300

    Eu nao aguento mais achar brechas nisso

--- Trabalhos/caixaEletronico.c
+++ Trabalhos/caixaEletronico.c
@@ -100,6 +100,36 @@ if(saque != 0){
     }        
 }
 
+if(saque != 0){
+        
+        saque = total;
+        quantidade100 = quantidade100 + contador100;
+        quantidade50 = quantidade50 + contador50;
+        quantidade20 = quantidade20 + contador20;
+        quantidade10 = quantidade10 + contador10;
+
+        contador100 = 0;
+        contador50 = 0;
+        contador20 = 0;
+        contador10 = 0;
+
+        while(saque > 60 && quantidade50 > 0){
+        saque = saque - 50;
+        quantidade50--;
+        contador50++;
+        }
+        while(saque >= 20 && quantidade20 > 0){
+        saque = saque - 20;
+        quantidade20--;
+        contador20++;
+    }
+        while(saque >= 10 && quantidade10 > 0){
+        saque = saque - 10;
+        quantidade10--;
+        contador10++;
+    }        
+}
+
     // printf("%d %d %d %d", contador100, contador50, contador20, contador10);
     // debugger dos contadores
 
